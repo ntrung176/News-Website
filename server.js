ce1b74502d998896c913354b86ae11ba123f2048
Ly Nguyen Nhat Trung <lynguyennhattrung1706@gmail.com>
2024-12-19 02:53:34 +0700

    fix view engine

--- server.js
+++ server.js
@@ -1,114 +1,299 @@
 const express = require("express");
-const postModel = require("./models/posts.model");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const hbs_sections = require("express-handlebars-sections");
+const bcrypt = require("bcryptjs");
+const moment = require("moment");
+require("express-async-errors");
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.engine(
+  "hbs",
+  exphbs.engine({
+    layoutsDir: "views/_layouts",
+    defaultLayout: "main",
+    extname: ".hbs",
+    helpers: {
+      section: hbs_sections(),
+    },
+  })
+);
+app.set("view engine", "hbs");
+
+app.set("trust proxy", 1); // trust first proxy
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      // secure: true
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/public", express.static("public"));
+
+app.use(function (req, res, next) {
+  if (req.isAuthenticated()) {
+    res.locals.lcIsAuthenticated = true;
+    res.locals.lcAuthUser = req.user;
+    if (req.user.Permission === 3) {
+      res.locals.lcAdmin = true;
+    }
+    if (req.user.Permission === 2) {
+      res.locals.lcEditor = true;
+    }
+    if (req.user.Permission === 1) {
+      res.locals.lcWriter = true;
+    }
+  }
+
+  next();
+});
+
 const categoryModel = require("./models/category.model");
 const subcategoryModel = require("./models/subcategory.model");
+const postModel = require("./models/posts.model");
 const _postModel = require("./models/_post.model");
-const router = express.Router();
-const moment = require("moment");
-const userModel = require("./models/user.model");
-const multer = require("multer");
 const commentModel = require("./models/comment.model");
-const expressHandlebarsSections = require("express-handlebars-sections");
 
-moment.locale("vi");
-
-router.get("/new", async function (req, res) {
-  const post = await _postModel.new();
-  for (var i = 0; i < post.length; i++) {
-    post[i].Time = moment(post[i].TimePost, "YYYY-MM-DD hh:mm:ss").fromNow();
-    const cat_post = await categoryModel.singleByCID(post[i].CID);
-    post[i].CName = cat_post.CName;
-    const subcat_post = await subcategoryModel.single2(post[i].SCID);
-    if (post[i].SCID !== null) {
-      post[i].SCName = subcat_post[0].SCName;
-    }
-    if (post[i].Premium === 1) {
-      post[i].Pre = true;
+app.use(async function (req, res, next) {
+  const rows = await categoryModel.allforuser();
+  for (var i = 0; i < rows.length; i++) {
+    if (i % 5 == 0) {
+      rows[i].xd = "margin-bottom: 45px;";
     }
   }
-
-  res.render("_vwPosts/new", {
-    post,
-  });
-});
-
-router.get("/hot", async function (req, res) {
-  const post = await _postModel.hot();
-  for (var i = 0; i < post.length; i++) {
-    post[i].Time = moment(post[i].TimePost, "YYYY-MM-DD hh:mm:ss").fromNow();
-    const cat_post = await categoryModel.singleByCID(post[i].CID);
-    post[i].CName = cat_post.CName;
-    const subcat_post = await subcategoryModel.single2(post[i].SCID);
-    if (post[i].Premium === 1) {
-      post[i].Pre = true;
-    }
-    if (post[i].SCID !== null) {
-      post[i].SCName = subcat_post[0].SCName;
-    }
+  for (var i = 0; i < rows.length; i++) {
+    const row = await subcategoryModel.singleforuser(rows[i].CID);
+    rows[i].subcategories = row;
   }
-
-  res.render("_vwPosts/hot", {
-    post,
-  });
+  res.locals.lcCategories = rows;
+  next();
 });
 
-router.get("/:id", async function (req, res) {
-  const id = +req.params.id || -1;
-  const pst = await postModel.singleByPostID(id);
-  const post = pst[0];
-  if (post.Premium === 1 && (req.isAuthenticated() || req.user.Premium !== 1)) {
-    const premium = true;
-    res.render("_vwPosts/baiviet", {
-      premium,
-    });
-  } else {
-    const ufullname = await userModel.singleByUserID(post.UID);
-    if (ufullname !== null) {
-      post.U_FullName = ufullname.Fullname;
-    }
-    post.Time = moment(post.TimePost, "YYYY-MM-DD hh:mm:ss").format(
-      "hh:mmA DD/MM/YYYY"
+app.use(async function (req, res, next) {
+  const post = await postModel.allPostSapPublic();
+  var today = new Date();
+  var date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  var time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var datetime = date + " " + time;
+  datetime = moment(datetime, "YYYY-MM-DD hh:mm:ss").format(
+    "YYYY-MM-DD hh:mm:ss"
+  );
+
+  for (var i = 0; i < post.length; i++) {
+    const entity = {
+      PostID: post[i].PostID,
+      Duyet: 3,
+      StatusPost: "Đã xuất bản",
+    };
+    datetimep = moment(post[i].TimePublic, "YYYY-MM-DD hh:mm:ss").format(
+      "YYYY-MM-DD hh:mm:ss"
     );
-    const comment = await commentModel.singleByPostID(id);
-    for (var i = 0; i < comment.length; i++) {
-      const u = await userModel.singleByUserID(comment[i].UID);
-      comment[i].username = u.UserName;
-      comment[i].Time = moment(
-        comment[i].Date,
+    if (datetimep <= datetime) {
+      await postModel.patch(entity);
+    }
+  }
+
+  next();
+});
+
+app.get("/", async function (req, res) {
+  const rows = await _postModel.best();
+  if (rows.length !== 0) {
+    const cat_post = await categoryModel.singleByCID(rows[0].CID);
+
+    const subcat_post = await subcategoryModel.single2(rows[0].SCID);
+    rows[0].CName = cat_post.CName;
+    if (rows[0].SCID !== null) {
+      rows[0].SCName = subcat_post[0].SCName;
+    }
+    rows[0].Time = moment(rows[0].TimePost, "YYYY-MM-DD hh:mm:ss").fromNow();
+    const hot = await _postModel.hot2();
+    const countComment = await commentModel.countByPostID(rows[0].PostID);
+    rows[0].countComment = countComment[0].Count;
+    for (var i = 0; i < hot.length; i++) {
+      const cat = await categoryModel.singleByCID(hot[i].CID);
+      const subc = await subcategoryModel.single2(hot[i].SCID);
+      hot[i].CName = cat.CName;
+      if (hot[i].SCID !== null) {
+        hot[i].SCName = subc[0].SCName;
+      }
+      hot[i].Time = moment(hot[i].TimePost, "YYYY-MM-DD hh:mm:ss").fromNow();
+    }
+    for (var i = 0; i < hot.length; i++) {
+      if (hot[i].PostID === rows[0].PostID) {
+        delete hot[i];
+      }
+    }
+
+    const new10 = await _postModel.new10();
+    for (var i = 0; i < new10.length; i++) {
+      const cat = await categoryModel.singleByCID(new10[i].CID);
+      const subc = await subcategoryModel.single2(new10[i].SCID);
+      new10[i].CName = cat.CName;
+      if (new10[i].SCID !== null) {
+        new10[i].SCName = subc[0].SCName;
+      }
+      new10[i].Time = moment(
+        new10[i].TimePost,
         "YYYY-MM-DD hh:mm:ss"
       ).fromNow();
+      if (new10[i].Premium === 1) {
+        new10[i].Pre = true;
+      }
     }
-    const tincungchuyenmuc = await _postModel.tincungchuyenmuc(post.SCID);
-    await _postModel.upview(id);
-    res.render("_vwPosts/baiviet", {
-      post,
-      comment,
-      tincungchuyenmuc,
-      empty: tincungchuyenmuc.length === 0,
+
+    const hot10 = await _postModel.hot10();
+    for (var i = 0; i < hot10.length; i++) {
+      const cat = await categoryModel.singleByCID(hot10[i].CID);
+      const subc = await subcategoryModel.single2(hot10[i].SCID);
+      hot10[i].CName = cat.CName;
+      if (hot10[i].SCID !== null) {
+        hot10[i].SCName = subc[0].SCName;
+      }
+      hot10[i].Time = moment(
+        hot10[i].TimePost,
+        "YYYY-MM-DD hh:mm:ss"
+      ).fromNow();
+      if (hot10[i].Premium === 1) {
+        hot10[i].Pre = true;
+      }
+    }
+
+    const category = await categoryModel.allforuser();
+    for (var i = 0; i < category.length; i++) {
+      const subcategory = await subcategoryModel.singleforuser(category[i].CID);
+      category[i].subcategory = subcategory;
+      const new1 = await _postModel.new1(category[i].CID);
+      category[i].new = new1;
+    }
+    res.render("home", {
+      rows,
+      hot,
+      new10,
+      hot10,
+      category,
     });
+  } else {
+    res.render("home");
   }
 });
 
-router.post("/:id", async function (req, res) {
-  const id = +req.params.id || -1;
-  req.body.UID = req.user.UserID;
-  req.body.PostID = id;
-  var today = new Date();
-  var time =
-    today.getFullYear() +
-    "-" +
-    (today.getMonth() + 1) +
-    "-" +
-    today.getDate() +
-    " " +
-    today.getHours() +
-    ":" +
-    today.getMinutes() +
-    ":" +
-    today.getSeconds();
-  req.body.Date = time;
-  await commentModel.add(req.body);
-  res.redirect("/post/" + id);
+app.get("/admin", function (req, res) {
+  if (req.isAuthenticated() && req.user.Permission === 3) {
+    res.render("adminpanel");
+  } else {
+    res.redirect("/");
+  }
 });
 
-module.exports = router;
+const categoryRouter = require("./routes/category.route");
+app.use("/admin/categories", categoryRouter);
+
+const subcategoryRouter = require("./routes/subcategory.route");
+app.use("/admin/subcategories", subcategoryRouter);
+
+const postRouter = require("./routes/posts.route");
+app.use("/admin/posts", postRouter);
+
+const userRouter = require("./routes/user.route");
+app.use("/", userRouter);
+
+const usersRouter = require("./routes/users.route");
+app.use("/admin/users", usersRouter);
+
+const utilsRouter = require("./routes/utils.route");
+app.use("/", utilsRouter);
+
+const _userModel = require("./routes/_user.router");
+app.use("/", _userModel);
+
+const _postRouter = require("./routes/_post.route");
+app.use("/post", _postRouter);
+
+const writerPanelRouter = require("./routes/writerpanel.route");
+app.use("/writerpanel", writerPanelRouter);
+
+const editorPanelRouter = require("./routes/editorpanel.route");
+app.use("/editorpanel", editorPanelRouter);
+
+const userModel = require("./models/user.model");
+
+app
+  .route("/dangnhap")
+  .get(function (req, res) {
+    res.render("_vwAccount/dangnhap");
+  })
+  .post(
+    passport.authenticate("local", {
+      failureRedirect: "/dangnhap",
+      successRedirect: "/",
+    })
+  );
+
+passport.use(
+  new LocalStrategy(async function (username, password, done) {
+    const user = await userModel.singleByUserName(username);
+    if (user === null) {
+      return done(null, false);
+    }
+    const rs = bcrypt.compareSync(password, user.Password_hash);
+    if (rs === false) {
+      return done(null, false);
+    }
+
+    return done(null, user);
+
+    delete user.Password_hash;
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.UserName);
+});
+
+passport.deserializeUser(async function (name, done) {
+  const user = await userModel.singleByUserName(name);
+  delete user.Password_hash;
+
+  if (user) {
+    return done(null, user);
+  } else {
+    return done(null, false);
+  }
+});
+
+app.get("/dangxuat", function (req, res) {
+  req.logout();
+  res.redirect(req.headers.referer);
+});
+
+app.use(function (req, res) {
+  res.render("404", { layout: false });
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).render("500", { layout: false });
+});
+
+const port = 3000;
+app.listen(port, () =>
+  console.log(`Server is running http://localhost:${port}`)
+);
