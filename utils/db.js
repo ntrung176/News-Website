49c16a371df7fbf133f91d9f8d516e206902204e
Ly Nguyen Nhat Trung <lynguyennhattrung1706@gmail.com>
2024-12-25 01:06:25 +0700

    env config

--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,12 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
-// Cấu hình kết nối từ biến môi trường
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "01012004",
-  database: process.env.DB_NAME || "web_data",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
 module.exports = {
